2afd46eee6a9b79689156dfea8827886f0178ae6
AnthonyNope <anthonynore@unisabana.edu.co>
2025-04-05 10:24:18 -0500

    Update Cliente.java

--- src/main/java/edu/unisabana/dyas/samples/entities/Cliente.java
+++ src/main/java/edu/unisabana/dyas/samples/entities/Cliente.java
@@ -6,45 +6,28 @@
 package edu.unisabana.dyas.samples.entities;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.sql.Date;
 
 /**
  *
  * @author cesarvefe
  */
-public class Item implements Serializable{
-    private TipoItem tipo;
+public class ItemRentado implements Serializable {
+
     private int id;
-    private String nombre;
-    private String descripcion;
-    private Date fechaLanzamiento;
-    private long tarifaxDia;
-    private String formatoRenta;
-    private String genero;
 
-    public Item(TipoItem tipo, int id, String nombre, String descripcion, Date fechaLanzamiento, long tarifaxDia, String formatoRenta, String genero) {
-        this.tipo = tipo;
+    private Item item;
+    private Date fechainiciorenta;
+    private Date fechafinrenta;
+
+    public ItemRentado(int id, Item item, Date fechainiciorenta, Date fechafinrenta) {
         this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.fechaLanzamiento = fechaLanzamiento;
-        this.tarifaxDia = tarifaxDia;
-        this.formatoRenta = formatoRenta;
-        this.genero = genero;
+        this.item = item;
+        this.fechainiciorenta = fechainiciorenta;
+        this.fechafinrenta = fechafinrenta;
     }
 
-    public Item() {
-    }
-
-    
-    
-    
-    public TipoItem getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoItem tipo) {
-        this.tipo = tipo;
+    public ItemRentado() {
     }
 
     public int getId() {
@@ -55,58 +38,35 @@ public class Item implements Serializable{
         this.id = id;
     }
 
-
-
-    public String getNombre() {
-        return nombre;
+    public Item getItem() {
+        return item;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setItem(Item item) {
+        this.item = item;
     }
 
-    public String getDescripcion() {
-        return descripcion;
+    public Date getFechainiciorenta() {
+        return fechainiciorenta;
     }
 
-    public void setDescrpcion(String descrpcion) {
-        this.descripcion = descrpcion;
+    public void setFechainiciorenta(Date fechainiciorenta) {
+        this.fechainiciorenta = fechainiciorenta;
     }
 
-    public Date getFechaLanzamiento() {
-        return fechaLanzamiento;
+    public Date getFechafinrenta() {
+        return fechafinrenta;
     }
 
-    public void setFechaLanzamiento(Date fechaLanzamiento) {
-        this.fechaLanzamiento = fechaLanzamiento;
-    }
-
-    public long getTarifaxDia() {
-        return tarifaxDia;
-    }
-
-    public void setTarifaxDia(long tarifaxDia) {
-        this.tarifaxDia = tarifaxDia;
-    }
-
-    public String getFormatoRenta() {
-        return formatoRenta;
-    }
-
-    public void setFormatoRenta(String formatoRenta) {
-        this.formatoRenta = formatoRenta;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
+    public void setFechafinrenta(Date fechafinrenta) {
+        this.fechafinrenta = fechafinrenta;
     }
 
     @Override
     public String toString() {
-        return "Item{" + "tipo=" + tipo + ", id=" + id + ", nombre=" + nombre + ", tarifaxDia=" + tarifaxDia + '}';
+        return "ItemRentado{" + "id=" + id + ", item=" + item + ", fechainiciorenta=" + fechainiciorenta + ", fechafinrenta=" + fechafinrenta + '}';
     }
+
+    
+    
 }
